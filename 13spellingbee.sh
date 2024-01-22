9c35b0d43d4aa62714d79766aa830aaad9a18c80
admafe <adelemferrer@gmail.com>
2024-01-21 19:00:48 -0800

    Week 2

--- 13spellingbee.sh
+++ 13spellingbee.sh
@@ -1,1 +1,3 @@
+cd ~/Code/MCB185/data
 gunzip -c dictionary.gz | grep -E "r" | grep -v "[^rzoniac]" | grep -E ".{4,}"
+gunzip -c dictionary.gz | grep -E "r" | grep -v "[^rzoniac]" | grep -E ".{4,}" | wc -l
