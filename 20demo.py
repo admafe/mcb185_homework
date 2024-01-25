57aada184242aac3c1857fd353f732f04146a11e
admafe <adelemferrer@gmail.com>
2024-01-25 01:45:42 -0800

    Update 1/25

--- 20demo.py
+++ 20demo.py
@@ -5,4 +5,18 @@ This is a
 multi-line
 comment
 """
+#Test Math 
+print(1.5e-2)
+print(7%2)
+print(5//2)
+import math
+print(math.sqrt(2))
 
+#Variables
+a=3
+b=4
+c= math.sqrt(a**2 + b**2)
+print(c)
+
+print(type(a), type(b), type(c))
+print(type(a), type(b), type(c), sep=", ")
