26c1dd24c4f6b93e0fe00b3b8782b1744f6fe114
admafe <adelemferrer@gmail.com>
2024-01-26 14:15:52 -0800

    update

--- 20demo.py
+++ 20demo.py
@@ -7,11 +7,15 @@ comment
 """
 #Test Math 
 import math
+
 print(1.5e-2)
 print(5%2)
 print(5//2)
 print(2**3)
 print(math.sqrt(2))
+print(math.ceil(37.5))
+print(math.pi)
+print(math.inf)
 print(pow(2,3))
 
 #Variables
@@ -22,3 +26,34 @@ print(c)
 
 print(type(a), type(b), type(c))
 print(type(a), type(b), type(c), sep=", ")
+
+#Functions
+def greeting():
+	print('hello yourself')
+	
+def pythagoras(a, b):
+	return math.sqrt(a**2 + b**2)
+	
+print(pythagoras(3,4))
+print(pythagoras(1,1))
+
+#def pythagoras(a, b):
+#	assert(a > 0)
+#	assert(b > 0)
+#	return math.sqrt(a**2 + b**2)
+#print(pythagoras(-1, 1))
+
+import sys
+#def pythagoras(a, b):
+#	if a <= 0: sys.exit('error: a must be greater than 0')
+#	if b <= 0: sys.exit('error: b must be greater than 0')
+#	return math.sqrt(a**2 + b**2)
+#print(pythagoras(2,-4))
+
+def inverse(a):
+	return -a
+print(inverse(-4))
+print(inverse(12))
+	
+s = 'hello world'
+print(s, type(s))
---
+++ 21quadratic.py
@@ -0,0 +1,5 @@
+#make sure to tell the program how to handle diff number of arguments
+# (return x1, x2), (return x,x), etc 
+
+def quadratic(a,b,c)
+	if (ax^2 + bx + c)
