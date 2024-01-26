96d25203f126f88722a4c59bed230ae6d5668eef
admafe <adelemferrer@gmail.com>
2024-01-26 15:02:50 -0800

    update

---
+++ 23hydropathy.py
@@ -0,0 +1,4 @@
+#chain the ifs
+#need to write out statements for each amino acid
+#want is it A, no then try next letter until through all aas
+#need a statemtn at end to return error message if input is not an AA letter code 
