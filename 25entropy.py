31898fe7142485badd56fbe041554cd77f4fcecb
admafe <adelemferrer@gmail.com>
2024-01-28 10:38:55 -0800

    update

--- 25entropy.py
+++ 25entropy.py
@@ -1,10 +1,11 @@
-# 25entropy.py by Adele Ferrer
-# Co-authors: David 
+# 25entropy.py by Adele Ferrer 
 
 # Take in counts, find freq of each, then do summation
 # Needs to account for and ignore 0 inputs (What if there is no T?)
 
-#each nucleotide has
+#		H = -(pA * math.log2(pA) + pC * math.log2(pC) + 
+#		      pG * math.log2(pG) + pT * math.log2(pT))
+
 import math
 import sys
 
@@ -14,19 +15,14 @@ def entropy(A, C, G, T):
 	pC = C / total
 	pG = G / total 
 	pT = T / total
-	if   A <= 0: H = -(pC * math.log2(pC) + pG * math.log2(pG) + pT * math.log2(pT))
-	elif C <= 0: H = -(pA * math.log2(pA) + pG * math.log2(pG) + pT * math.log2(pT))
-	elif G <= 0: H = -(pA * math.log2(pA) + pC * math.log2(pC) + pT * math.log2(pT))
-	elif T <= 0: H = -(pA * math.log2(pA) + pC * math.log2(pC) + pG * math.log2(pG))
-	else:
-		H = -(pA * math.log2(pA) + pC * math.log2(pC) + 
-		      pG * math.log2(pG) + pT * math.log2(pT))
-	return H
+	H = 0
+	if A > 0: H = H + pA * math.log2(pA)
+	if C > 0: H = H + pC * math.log2(pC)
+	if G > 0: H = H + pG * math.log2(pG)
+	if T > 0: H = H + pT * math.log2(pT)
+	return -H
 
 print(entropy(1, 1, 1, 1))
 print(entropy(30, 14, 17, 12))
 print(entropy(4, 0, 5, 7))
-#print(entropy(5, 0, 6, 0))
-
-#Note to self: cannot handle more than 1 zero input
-#Figure out how to format to ignore parts of fxn when input is 0
+print(entropy(5, 0, 6, 0))
