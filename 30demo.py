caf837887b5f71d82bad5c96fd646996f02464dd
admafe <adelemferrer@gmail.com>
2024-01-30 00:52:13 -0800

    update

--- 30demo.py
+++ 30demo.py
@@ -14,11 +14,29 @@
 #	i = i + 1
 #print('final value of i is', i)
 
-for i in range(1, 10, 3):
-	print (i)
+#for i in range(1, 10, 3):
+#	print (i)
 	
-for i in range(0, 5):
-	print (i)
+#for i in range(0, 5):
+#	print (i)
 	
-for i in range(5):
-	print (i)
+#for i in range(5):
+#	print (i)
+	
+#for char in 'hello':
+#	print(char)
+	
+#seq = 'GAATTC'
+#for nt in seq:
+#	print (nt)
+
+#for nt1 in 'ACGT':
+#	for nt2 in 'ACGT':
+#		if nt1 == nt2: print(nt1, nt2, '+1')		
+#		else:          print(nt1, nt2, '-1')
+		
+nts = 'ACGT'
+for nt1 in nts:
+	for nt2 in nts:
+		if nt1 == nt2: print(nt1, nt2, '+1')		
+		else:          print(nt1, nt2, '-1')
--- 32fibonacci.py
+++ 32fibonacci.py
@@ -2,9 +2,14 @@
 
 #need to set range, then add each number set, and loop through each new number
 
-def fibonacci(n):
-	sequence = [0,1]
-	if n <= 1:
+#def fibonacci(n):
+#	sequence = [0,1]
+#	if n <= 1:
 
-for i in range(2,11):
-	
+for i in range(0,21):
+	n1 = i
+	n2 = n2 + n1	
+	print(n2)
+
+#Need a variable to hold first argument, then loop through that variable
+# Check notes for more 
