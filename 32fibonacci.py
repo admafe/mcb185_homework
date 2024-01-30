27de82f5e6d3c26ed4a533b3b7ed75a31a5ad08e
admafe <adelemferrer@gmail.com>
2024-01-30 09:03:49 -0800

    update

--- 32fibonacci.py
+++ 32fibonacci.py
@@ -2,14 +2,21 @@
 
 #need to set range, then add each number set, and loop through each new number
 
-#def fibonacci(n):
-#	sequence = [0,1]
-#	if n <= 1:
+def fibonacci(n):
+	n = 0
+	for i in range(0,21):
+		n1 = n + i
+		n2 = n1 + n
+	return (n2)
+	
+print(fibonacci(1))
+print(fibonacci(9))
 
-for i in range(0,21):
-	n1 = i
-	n2 = n2 + n1	
-	print(n2)
+
+#for i in range(0,21):
+#	n1 = i
+#	n2 = n2 + n1	
+#	print(n2)
 
 #Need a variable to hold first argument, then loop through that variable
 # Check notes for more 
---
+++ 34scoringmatrix.py
@@ -0,0 +1,6 @@
+# 34scoringmatrix.py by Adele Ferrer
+
+# A = +1
+# C = -1
+# G = -1
+# T = -1
