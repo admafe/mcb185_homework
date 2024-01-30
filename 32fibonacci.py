8122145615f83dc1a13da0c82d3bc86f7506013b
admafe <adelemferrer@gmail.com>
2024-01-29 22:15:44 -0800

    update

---
+++ 32fibonacci.py
@@ -0,0 +1,10 @@
+# 32fibonacci.py by Adele Ferrer
+
+#need to set range, then add each number set, and loop through each new number
+
+def fibonacci(n):
+	sequence = [0,1]
+	if n <= 1:
+
+for i in range(2,11):
+	
