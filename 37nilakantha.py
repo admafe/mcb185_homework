9264f5a4f6c32cb69ee76c523198ca5a31499cfa
admafe <adelemferrer@gmail.com>
2024-02-03 22:52:44 -0800

    update

--- 37nilakantha.py
+++ 37nilakantha.py
@@ -2,15 +2,18 @@
 
 def nilakantha(n):
 	x = 3
+	sign = 1
 	for i in range(2, n, 3):
-		denom = ((i) * (i + 1) * (i + 2))
-		if i % 2 == 0: return(+denom)
-		else:        return((-1)*denom)
-	pi = x + (4/denom)
+		denom = (i) * (i + 1) * (i + 2)
+		sign = sign * -1
+#		if i % 2 == 0: return(+denom)
+#		else:        return((-1)*denom)
+	pi = x + (sign * (4/denom))
 	return (pi)
 		
 print(nilakantha(5))
 print(nilakantha(21))
+print(nilakantha(100))
 	
 # set range, flip flop, for every even = +
 # for every odd = -
