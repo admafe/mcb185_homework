f53de8f3456d308f2a8e639694f87f0245a18bf6
admafe <adelemferrer@gmail.com>
2024-02-03 22:28:52 -0800

    update

--- 37nilakantha.py
+++ 37nilakantha.py
@@ -3,8 +3,8 @@
 def nilakantha(n):
 	x = 3
 	for i in range(2, n, 3):
-		denom = (i) * (i + 1) * (i + 2)
-		if i%2 == 0: return(+denom)
+		denom = ((i) * (i + 1) * (i + 2))
+		if i % 2 == 0: return(+denom)
 		else:        return((-1)*denom)
 	pi = x + (4/denom)
 	return (pi)
