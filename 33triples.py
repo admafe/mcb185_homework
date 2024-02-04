3e01eb932d1f0a2669453bb88f372db0557b7b3b
admafe <adelemferrer@gmail.com>
2024-02-03 16:19:58 -0800

    update

--- 33triples.py
+++ 33triples.py
@@ -7,14 +7,26 @@
 # is integer 
 
 #Needs loop inside a loop; nested loop 
-
-def is_triples(a, b, c):
+"""
+def triples(a, b):
 	if a**2 + b**2 == c**2:
 		if c == c//1:
-			return True
-	else: return False 
+			return (c)
 	
-print(is_triples(3, 4, 5))
+"""
+"""	
+print(is_triples(3, 4, 5)) #yes
 print(is_triples(1, 2, 3))
-print(is_triples(5, 12, 13))
+print(is_triples(5, 12, 13)) #yes
 print(is_triples(4, 7, 12))
+"""
+
+import math
+
+limit = 100
+for a in range(1, limit): 
+	for b  in range(a + 1, limit): 
+		c = math.sqrt(a**2 + b**2)
+		if c == c//1: print(a, b, c)
+if a != b: print(a, b, c)
+			
