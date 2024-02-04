e6c2546dbe7db2a68ed0b8f3c01bee5b34586684
admafe <adelemferrer@gmail.com>
2024-02-03 18:30:01 -0800

    update

--- 34scoringmatrix.py
+++ 34scoringmatrix.py
@@ -1,14 +1,14 @@
 # 34scoringmatrix.py by Adele Ferrer
+# Co-author: George M
 
 #just keep printing rows at a time and cut the ending so it doesn't go to new row
 # use "end = " so it ends with space not return
 
 nts = 'ACGT'
+print ('   A  C  G  T') #title line
 for nt1 in nts:
-	nt1 = nt1 + 1
-	print(nt1, end = ' ')
-"""
+	print (nt1, end = ' ') #print nt at beginning of each loop/ line
 	for nt2 in nts:
-		if nt1 == nt2: print(nt1, nt2, '+1', end = ' ')		
-		else:          print(nt1, nt2, '-1', end = ' ')
-"""
+		if nt1 == nt2: print('+1', end = ' ')		
+		else:          print('-1', end = ' ')
+	print() #print
--- 37nilakantha.py
+++ 37nilakantha.py
@@ -1,13 +1,20 @@
 # 37nilakantha.py by Adele Ferrer
 
 def nilakantha(n):
-	pi = 3
-	for i in range(n):
-		i = i + 1
-		return i 
-		i = i + 2
-		return i
+	x = 3
+	for i in range(2, n):
+		denom = (i) * (i + 1) * (i + 2)
+		if i%2 == 0: return(denom)
+		else:        return((-1)*denom)
+	pi = x + 4/denom
+	return (pi)
 		
 print(nilakantha(5))
+print(nilakantha(21))
 	
-	
+# set range, flip flop, for every even = +
+# for every odd = -
+"""	
+if i%2 == 0: +
+if i%2 == 1: -
+"""
