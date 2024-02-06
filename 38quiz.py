6e24ee7effa8aaadea0502808892149cee783671
admafe <156118246+admafe@users.noreply.github.com>
2024-02-06 09:26:30 -0800

    update

--- 38quiz.py
+++ 38quiz.py
@@ -1,2 +1,18 @@
 # 38quiz.py by Adele Ferrer
 # Co-author: Marle Lamountry
+
+#Task 1
+def pi_estimate(n):
+	x = 1
+	sign = -1
+	for n in range(3, n, 2): 
+		y = (sign * (1 / n))
+		x = x + y
+		sign = sign * -1
+		pi = 4 * (x)
+	return(pi)
+		
+print(pi_estimate(16))
+print(pi_estimate(100))
+
+#Task
