288fc6abca15e840109cd1d37d4eaea51345fac8
admafe <156118246+admafe@users.noreply.github.com>
2024-02-11 10:54:00 -0800

    update

--- 47deathsaves.py
+++ 47deathsaves.py
@@ -2,7 +2,7 @@
 
 import random
 
-rolls = 100000
+rolls = 10000
 
 deaths = 0
 stable = 0
@@ -10,15 +10,24 @@ revives = 0
 for i in range(rolls):
 	failures = 0
 	successes = 0
-	for j in range(3):
+	while True:
 		d1 = random.randint(1, 20)
-		if 1 < d1 < 10:   failures += 1
-		if 20 > d1 >= 10: successes += 1
-		if d1 == 1:       failures += 2
-		if d1 == 20:      revives += 1
+		if d1 == 20:
+			revives += 1 
+			break
+		elif d1 >= 10:
+			successes += 1
+		elif d1 == 1:
+			failures += 2
+		else:
+			failures += 1
+		if failures >= 3:
+			deaths += 1
+			break
+		elif successes >= 3:
+			stable += 1
+			break
 #		print(d1)
 #	print(f'Failures: {failures}', f'Successes: {successes}')
-	if failures >= 3: deaths += 1
-	if successes == 3: stable += 1
 
 print(deaths/rolls, stable/rolls, revives/rolls)
